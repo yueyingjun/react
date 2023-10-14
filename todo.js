7b8c9a0384e280e6d79953969022f9a4fc37f5a3
Vidhi <vidhijain2704@gmail.com>
2023-10-14 15:36:06 +0530

    vidhi changed somethings

--- todo.js
+++ todo.js
@@ -6,6 +6,4 @@ var _react2 = _interopRequireDefault(_react);
 
 var _reactDom = require("react-dom");
 
-var _reactDom2 = _interopRequireDefault(_reactDom);
-
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
